d3c03041acb55af6dd7213a4bf79d2d2a4bb1d57
mihmosi <mihmosi@gmail.com>
2020-11-22 12:49:08 +0300

    mobile toggle sidebar

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
-document.querySelector('.menu-icon-wrapper').onclick = function () {
-    document.querySelector('.menu-icon').classList.toggle('menu-icon-active');
+const sidebarToggleBtn = document.querySelector('.menu-icon-wrapper');
+const menuIcon = document.querySelector('.menu-icon');
+const sidebar = document.querySelector('.sidebar')
+
+
+
+
+sidebarToggleBtn.onclick = function () {
+    menuIcon.classList.toggle('menu-icon-active');
+    sidebar.classList.toggle('sidebar--mobile-active');
 }
+
+// sidebar--mobile-active
